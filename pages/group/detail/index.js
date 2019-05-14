1047a693faee5a21eccb186919dde1f6093429f8
MrNeptune <ywj-1991@qq.com>
2019-05-14 08:55:07 +0800

    c
    c

--- pages/group/detail/index.js
+++ pages/group/detail/index.js
@@ -37,7 +37,11 @@ Page({
     web.request("C1015", {
       "id": id,
     }, {
-      success: function(data) {
+      success: function(data,code) {
+        if(code == 1001){
+          this.fail(code,"拼团活动已经结束了!");
+          return;
+        }
         data.state = "show";
 
         let d1Arr = data.getTimeStart.split(" ")[0].split("-");
